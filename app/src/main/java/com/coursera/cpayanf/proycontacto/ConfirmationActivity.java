383b2619678fec9662c2fe546f3002e33bf3f4e5
Carlos Payan <carlos.payan@delaware-software.com>
2018-02-16 17:30:02 -0600

    Código

--- app/src/main/java/com/coursera/cpayanf/proycontacto/ConfirmationActivity.java
+++ app/src/main/java/com/coursera/cpayanf/proycontacto/ConfirmationActivity.java
@@ -1,13 +1,86 @@
 package com.coursera.cpayanf.proycontacto;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ConfirmationActivity extends AppCompatActivity {
+	TextView lblNombre;
+	TextView lblTelefono;
+	TextView lblEmail;
+	TextView lblDescripcion;
+	TextView lblFecNac;
+	Button btnEditar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_confirmation);
+
+		try {
+			CreaBtnEditar();
+
+			lblNombre = findViewById(R.id.lblValNombre);
+			lblTelefono = findViewById(R.id.lblValTelefono);
+			lblEmail = findViewById(R.id.lblValEmail);
+			lblFecNac = findViewById(R.id.lblValFecNac);
+			lblDescripcion = findViewById(R.id.lblValDescripcion);
+
+			Bundle loParams = getIntent().getExtras();
+			String lsNombre = loParams.getString("psNombreContacto");
+			String lsTelefono = loParams.getString("psTelefonoContacto");
+			String lsEmail = loParams.getString("psEmailContacto");
+			String lsFecNac = loParams.getString("psFecNacContacto");
+			String lsDescripcion = loParams.getString("psDescripcionContacto");
+
+			lblNombre.setText(lsNombre);
+			lblTelefono.setText(lsTelefono);
+			lblEmail.setText(lsEmail);
+			lblFecNac.setText(lsFecNac);
+			lblDescripcion.setText(lsDescripcion);
+		}
+		catch (Exception ex) {
+			Toast.makeText(getBaseContext(), getResources().getString(R.string.MainActCreateErr), Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	protected void CreaBtnEditar(){
+		btnEditar = findViewById(R.id.btnEditar);
+		btnEditar.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				try{
+					Intent loIntent = new Intent(ConfirmationActivity.this, MainActivity.class);
+					loIntent.putExtra("psNombreContacto", lblNombre.getText().toString());
+					loIntent.putExtra("psTelefonoContacto", lblTelefono.getText().toString());
+					loIntent.putExtra("psEmailContacto", lblEmail.getText().toString());
+					loIntent.putExtra("psFecNacContacto", lblFecNac.getText().toString());
+					loIntent.putExtra("psDescripcionContacto", lblDescripcion.getText().toString());
+					startActivity(loIntent);
+					finish();
+				}
+				catch (Exception ex) {
+					Toast.makeText(getBaseContext(), ex.getMessage(), Toast.LENGTH_LONG).show();
+				}
+			}
+		});
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event)
+	{
+		if(keyCode == KeyEvent.KEYCODE_BACK)
+		{
+			Intent loIntent = new Intent(ConfirmationActivity.this, MainActivity.class);
+			startActivity(loIntent);
+			finish();
+		}
+		return super.onKeyDown(keyCode, event);
+
 	}
 }
--- app/src/main/java/com/coursera/cpayanf/proycontacto/MainActivity.java
+++ app/src/main/java/com/coursera/cpayanf/proycontacto/MainActivity.java
@@ -1,13 +1,92 @@
 package com.coursera.cpayanf.proycontacto;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+	EditText txtNombre;
+	EditText txtTelefono;
+	DatePicker dtpFechaNacimiento;
+	EditText txtEmail;
+	EditText txtDescripcion;
+	Button btnSiguiente;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		String lsNombre;
+		String lsTelefono;
+		String lsEmail;
+		String lsFecNac;
+		String lsDescripcion;
+		String[] lsFecha;
+		try
+		{
+			CreaBtnSiguiente();
+
+			txtNombre = findViewById(R.id.txtValNombre);
+			txtTelefono = findViewById(R.id.txtValTelefono);
+			txtEmail = findViewById(R.id.txtValEmail);
+			dtpFechaNacimiento = findViewById(R.id.dtpFechaNacimiento);
+			txtDescripcion = findViewById(R.id.txtValDescripcion);
+
+			txtNombre.setSelectAllOnFocus(true);
+			txtTelefono.setSelectAllOnFocus(true);
+			txtEmail.setSelectAllOnFocus(true);
+			txtDescripcion.setSelectAllOnFocus(true);
+
+			Bundle loParams = getIntent().getExtras();
+			lsNombre = loParams.getString("psNombreContacto");
+			lsTelefono = loParams.getString("psTelefonoContacto");
+			lsEmail = loParams.getString("psEmailContacto");
+			lsFecNac = loParams.getString("psFecNacContacto");
+			lsDescripcion = loParams.getString("psDescripcionContacto");
+
+			lsFecha = lsFecNac.split("-");
+			txtNombre.setText(lsNombre);
+			txtTelefono.setText(lsTelefono);
+			txtEmail.setText(lsEmail);
+			dtpFechaNacimiento.updateDate(Integer.parseInt(lsFecha[2]),
+					Integer.parseInt(lsFecha[1]),
+					Integer.parseInt(lsFecha[0]));
+			txtDescripcion.setText(lsDescripcion);
+		}
+		catch (Exception ex) {
+			Toast.makeText(getBaseContext(), getResources().getString(R.string.MainActCreateErr), Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	protected void CreaBtnSiguiente(){
+		btnSiguiente = findViewById(R.id.btnSiguiente);
+		btnSiguiente.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				try {
+					Intent loIntent = new Intent(MainActivity.this, ConfirmationActivity.class);
+					loIntent.putExtra("psNombreContacto", txtNombre.getText().toString());
+					loIntent.putExtra("psTelefonoContacto", txtTelefono.getText().toString());
+					loIntent.putExtra("psEmailContacto", txtEmail.getText().toString());
+					loIntent.putExtra("psFecNacContacto", "" + dtpFechaNacimiento.getDayOfMonth()
+							+ "-" + (dtpFechaNacimiento.getMonth() + 1)
+							+ "-" + dtpFechaNacimiento.getYear());
+					loIntent.putExtra("psDescripcionContacto", txtDescripcion.getText().toString());
+					startActivity(loIntent);
+					finish();
+				}
+				catch (Exception ex)
+				{
+					Toast.makeText(getBaseContext(), ex.getMessage(), Toast.LENGTH_LONG).show();
+				}
+			}
+		});
 	}
 }
